f76cd9cac733db47d812f865f384cd10d4cefd73
ickeyben123 <rushbyalfie99@gmail.com>
2025-02-26 11:28:47 +0000

    Add VPC module

--- providers.tf
+++ providers.tf
@@ -2,4 +2,12 @@ provider "aws" {
   region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
+  default_tags {
+    tags = {
+      Environment = "Testing"
+      managed_by  = "Terraform"
+      Cost_Tag    = "AlfiesIssueCreator"
+      Project     = "AlfiesIssueCreator"
+    }
+  }
 }
