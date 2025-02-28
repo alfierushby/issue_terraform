fc79615f1274126390d37c31161d11c0850b69e9
ickeyben123 <rushbyalfie99@gmail.com>
2025-02-28 11:53:16 +0000

    Remove ecr role as not needed

--- modules/iam/iam.tf
+++ modules/iam/iam.tf
@@ -76,23 +76,6 @@ module "iam_eks_role" {
 } 
 
 
-module "iam_eks_role_ecr" {
-  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  role_name = "alfie-role-ecr"
-
-    role_policy_arns = {
-    AmazonEKS_CNI_Policy = aws_iam_policy.ecr.arn
-  }
-
-  oidc_providers = {
-    one = {
-      provider_arn               = var.oidc_provider_arn
-      namespace_service_accounts = ["alfie:ecr"]
-    }
-  }
-}
-
-
 module "iam_eks_role_lb" {
   source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   role_name = "alfie-iam_eks_role_lb_controller"
